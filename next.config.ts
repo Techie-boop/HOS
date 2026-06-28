d8da003a004c60452fa4837e8136f85824be01e3
Your Name <help.ckrdatapoint@gmail.com>
2026-06-28 13:40:33 +0530

    chore: add outputFileTracingIncludes to force pg-cloudflare trace resolution in wrangler

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**",
+    ],
+  },
 };
 
 export default nextConfig;
